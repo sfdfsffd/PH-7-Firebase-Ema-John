0a35e385891923a821e9caa21bef41a2d80811b7
Touhidur Zaman <touhidcodes@gmail.com>
2023-04-16 22:19:03 +0600

    display-items

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -6,6 +6,16 @@ const ReviewItem = ({ product }) => {
 	return (
 		<div className='Item'>
 			<img src={img} alt='' />
+			<div className='review-details'>
+				<p>{name}</p>
+				<p>
+					Price: <span className='text-orange'>${price}</span>
+				</p>
+				<p>
+					Order Quantity: <span className='text-orange'>{quantity}</span>
+				</p>
+			</div>
+			<button className='btn-delete'>Del</button>
 		</div>
 	);
 };
